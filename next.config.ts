70b27e9fcf79d99b011390527af7757587670f1b
jiHwan <ghkdrbqhd35@gmail.com>
2025-07-04 17:51:46 +0900

    초기설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['@radix-ui/react-slot'],
+  },
 };
 
 export default nextConfig;
